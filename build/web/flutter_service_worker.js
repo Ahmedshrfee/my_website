ad53ac1391204b08ee940541a062a74e67b32624
Ahmedshrfee <Ahmadshrfee@gmail.com>
2025-12-01 04:07:50 +0300

    update the bulid

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3f0aa8170045945720d99809c4f079e3
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "160e15266f593928531f3203a58eaf0d",
+"flutter_bootstrap.js": "04059d79787cecd040631207052c4c90",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
