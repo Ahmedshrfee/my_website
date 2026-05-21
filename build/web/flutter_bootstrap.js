2de8fa5f041121642d49a627b5ae40c688d53bd0
Ahmedshrfee <Ahmadshrfee@gmail.com>
2026-05-22 01:15:37 +0300

    change my whatsapp number

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3500718017"
+    serviceWorkerVersion: "2990250215"
   }
 });
